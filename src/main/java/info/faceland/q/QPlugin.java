cf90021e5c515eefa8b79d31971ffda0e92849f1
Richard Harrah <topplethenunnery@gmail.com>
2014-09-23 15:33:51 -0400

    using debug printer

--- src/main/java/info/faceland/q/QPlugin.java
+++ src/main/java/info/faceland/q/QPlugin.java
@@ -2,12 +2,16 @@ package info.faceland.q;
 
 import info.faceland.api.FacePlugin;
 import info.faceland.q.actions.questions.QuestionManager;
+import net.nunnerycode.java.libraries.cannonball.DebugPrinter;
 import org.bukkit.event.HandlerList;
+
+import java.util.logging.Level;
 
 public class QPlugin extends FacePlugin {
 
     private QuestionManager questionManager;
     private QListener qListener;
+    private DebugPrinter debugPrinter;
 
     @Override
     public void preEnable() {
@@ -16,6 +20,7 @@ public class QPlugin extends FacePlugin {
 
     @Override
     public void enable() {
+        debugPrinter = new DebugPrinter(getDataFolder().getName(), "debug.log");
         questionManager = new QuestionManager();
         qListener = new QListener(this);
         getServer().getPluginManager().registerEvents(qListener, this);
@@ -45,6 +50,12 @@ public class QPlugin extends FacePlugin {
 
     public QuestionManager getQuestionManager() {
         return questionManager;
+    }
+
+    public void debug(Level level, String... message) {
+        if (debugPrinter != null) {
+            this.debugPrinter.debug(level, message);
+        }
     }
 
 }
